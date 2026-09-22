e535e97bdd04999c9325c0d71f44b1cbc512652a
MAP01 <maguilera131113@gmail.com>
2026-09-22 05:47:39 -0700

    Create main.cpp

---
+++ vCPP/basic-structure/main.cpp
@@ -0,0 +1,19 @@
+/*
+
+Programmers {
+  1. Mario Aguilera Piceno
+}
+
+File: main.cpp
+About: A basic structured stack example.
+Created: 09/22/2026
+Last Modified: 09/22/2026
+
+*/
+
+#include <iostream>
+#include <stack>
+
+int main() {
+  
+}
